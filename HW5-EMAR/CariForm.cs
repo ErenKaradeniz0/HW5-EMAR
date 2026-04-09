5eba25b12c7f046b993e7d01c0a30110f637de49
Eren Karadeniz <83872752+ErenKaradeniz0@users.noreply.github.com>
2026-04-09 21:36:46 +0300

    Add company info fields to CariForm and reorganize layout

--- HW5-EMAR/CariForm.cs
+++ HW5-EMAR/CariForm.cs
@@ -4,66 +4,63 @@ namespace HW5_EMAR
 {
     public class CariForm : Form
     {
+        TextBox CompanyNameTextBox;
+        TextBox CompanyTaxIdTextBox;
         TextBox NameSurnameTextBox;
-        TextBox AddressTextBox;
         TextBox PhoneTextBox;
+        TextBox CityTextBox;
+        TextBox DistrictTextBox;
+        TextBox AddressTextBox;
+        TextBox CategoryTextBox;
 
         public CariForm()
         {
-            NameSurnameTextBox = new TextBox
-            {
-                size = new Size(20, 3),
-                location = new Point(25, 0),
-            };
-            AddressTextBox = new TextBox
-            {
-                size = new Size(20, 3),
-                location = new Point(25, 4),
-            };
-            PhoneTextBox = new TextBox
-            {
-                size = new Size(20, 3),
-                location = new Point(25, 8),
-            };
-            LabelBox NameSurnameLabel = new LabelBox
-            {
-                size = new Size(20, 3),
-                location = new Point(5, 0),
-                Value = "Name Surname"
-            };
-            LabelBox AddressLabel = new LabelBox
-            {
-                size = new Size(20, 3),
-                location = new Point(5, 4),
-                Value = "Address"
-            };
-            LabelBox PhoneLabel = new LabelBox
-            {
-                size = new Size(20, 3),
-                location = new Point(5, 8),
-                Value = "Phone"
-            };
-            ButtonBox saveButton = new ButtonBox
-            {
-                size = new Size(20, 3),
-                location = new Point(25, 14),
-                Value = "Submit"
-            };
+            LabelBox CompanyNameLabel = new LabelBox { size = new Size(20, 3), location = new Point(5, 0), Value = "Firma Adi" };
+            CompanyNameTextBox = new TextBox { size = new Size(20, 3), location = new Point(25, 0) };
+
+            LabelBox CompanyTaxIdLabel = new LabelBox { size = new Size(20, 3), location = new Point(5, 3), Value = "Firma VKN" };
+            CompanyTaxIdTextBox = new TextBox { size = new Size(20, 3), location = new Point(25, 3) };
+
+            LabelBox NameSurnameLabel = new LabelBox { size = new Size(20, 3), location = new Point(5, 6), Value = "Ad Soyad" };
+            NameSurnameTextBox = new TextBox { size = new Size(20, 3), location = new Point(25, 6) };
+
+            LabelBox PhoneLabel = new LabelBox { size = new Size(20, 3), location = new Point(5, 9), Value = "Telefon" };
+            PhoneTextBox = new TextBox { size = new Size(20, 3), location = new Point(25, 9) };
+
+            LabelBox CityLabel = new LabelBox { size = new Size(20, 3), location = new Point(5, 12), Value = "Il" };
+            CityTextBox = new TextBox { size = new Size(20, 3), location = new Point(25, 12) };
+
+            LabelBox DistrictLabel = new LabelBox { size = new Size(20, 3), location = new Point(5, 15), Value = "Ilce" };
+            DistrictTextBox = new TextBox { size = new Size(20, 3), location = new Point(25, 15) };
+
+            LabelBox AddressLabel = new LabelBox { size = new Size(20, 3), location = new Point(5, 18), Value = "Adres" };
+            AddressTextBox = new TextBox { size = new Size(20, 3), location = new Point(25, 18) };
+
+            LabelBox CategoryLabel = new LabelBox { size = new Size(20, 3), location = new Point(5, 21), Value = "Kategori" };
+            CategoryTextBox = new TextBox { size = new Size(20, 3), location = new Point(25, 21) };
+
+            ButtonBox saveButton = new ButtonBox { size = new Size(20, 3), location = new Point(25, 25), Value = "Submit" };
             saveButton.Action += SaveButtonClicked;
 
-            ButtonBox cancelButton = new ButtonBox
-            {
-                size = new Size(20, 3),
-                location = new Point(45, 14),
-                Value = "Cancel"
-            };
+            ButtonBox cancelButton = new ButtonBox { size = new Size(20, 3), location = new Point(45, 25), Value = "Cancel" };
             cancelButton.Action += CancelButtonClicked;
-            Boxes.Add(NameSurnameTextBox);
-            Boxes.Add(AddressTextBox);
-            Boxes.Add(PhoneTextBox);
+
+            Boxes.Add(CompanyNameLabel);
+            Boxes.Add(CompanyNameTextBox);
+            Boxes.Add(CompanyTaxIdLabel);
+            Boxes.Add(CompanyTaxIdTextBox);
             Boxes.Add(NameSurnameLabel);
-            Boxes.Add(AddressLabel);
+            Boxes.Add(NameSurnameTextBox);
             Boxes.Add(PhoneLabel);
+            Boxes.Add(PhoneTextBox);
+            Boxes.Add(CityLabel);
+            Boxes.Add(CityTextBox);
+            Boxes.Add(DistrictLabel);
+            Boxes.Add(DistrictTextBox);
+            Boxes.Add(AddressLabel);
+            Boxes.Add(AddressTextBox);
+            Boxes.Add(CategoryLabel);
+            Boxes.Add(CategoryTextBox);
             Boxes.Add(saveButton);
             Boxes.Add(cancelButton);
         }
@@ -71,11 +68,16 @@ namespace HW5_EMAR
         public void SaveButtonClicked()
         {
             //Console.WriteLine("Save button clicked");
+            var companyName = CompanyNameTextBox.Value;
+            var companyTaxId = CompanyTaxIdTextBox.Value;
             var nameSurname = NameSurnameTextBox.Value;
-            var adress = AddressTextBox.Value;
             var phone = PhoneTextBox.Value;
-
+            var city = CityTextBox.Value;
+            var district = DistrictTextBox.Value;
+            var address = AddressTextBox.Value;
+            var category = CategoryTextBox.Value;
         }
+
         public void CancelButtonClicked()
         {
             Console.WriteLine("Cancel button clicked");
