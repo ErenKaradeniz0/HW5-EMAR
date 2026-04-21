9220dc0dcc2725a535d7f9e60293804e3fbb4ebd
ErenKaradeniz0 <erenkaradeniz011@gmail.com>
2026-04-21 21:00:33 +0300

    Add TODO comment outlining planned application features
    Added a multi-line TODO comment in the Main method to document planned features, including main page creation, record listing and management, and automatic record keeping. No changes to application functionality were made.

--- HW5-EMAR/Program.cs
+++ HW5-EMAR/Program.cs
@@ -6,6 +6,15 @@ namespace HW5_EMAR
     {
         static void Main(string[] args)
         {
+            /*
+             TODO: HW7
+               0 - ana sayfa oluşturma (cari listele cari olutuştur seçeneği olmalı)
+               1 - cari kayıtları listeleme özelliğ
+                    1.1 - listeden kayıt seçip silme özelliği
+                    1.2 - listeden kayıt seçip güncelleme özelliği
+               2 - cari oluşturma özelliği
+                    2.1 - kayıtları otomatik tutma
+             */
             Console.CursorVisible = false;
             CariForm cariForm = new CariForm();
             cariForm.Show();
