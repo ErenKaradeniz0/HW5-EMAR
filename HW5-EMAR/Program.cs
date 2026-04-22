27c1eeae87a556679fd075231ae11924ef40d7c9
Eren Karadeniz <erenkaradeniz011@gmail.com>
2026-04-22 15:56:07 +0300

    Remove obsolete TODO comments from Program.cs

--- HW5-EMAR/Program.cs
+++ HW5-EMAR/Program.cs
@@ -6,11 +6,6 @@ namespace HW5_EMAR
     {
         static void Main(string[] args)
         {
-            /*
-             TODO: HW7
-                1.1 - listeden kayıt seçip silme özelliği
-                1.2 - listeden kayıt seçip güncelleme özelliği
-             */
             Console.CursorVisible = false;
             MainForm mainForm = new MainForm();
             mainForm.Show();
